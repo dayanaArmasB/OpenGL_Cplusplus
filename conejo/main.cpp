c0de6c4afd7c41017c2647a60898fb72c7e97063
Dayana Armas <dyarmasb@hotmail.com>
2023-09-01 20:35:34 -0500

    se agrego nuevo proyecto

---
+++ conejo/main.cpp
@@ -0,0 +1,378 @@
+#include <GL/glut.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void conejo() {
+	
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glLineWidth(3.0f);
+	
+	glBegin(GL_LINE_LOOP);
+
+    glVertex2f(328.0f, 167.0f); // Ajusta la coordenada x
+    glVertex2f(368.0f, 120.0f); // Ajusta la coordenada x
+    glVertex2f(415.0f, 228.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+
+    glVertex2f(527.0f, 132.0f); // Ajusta la coordenada x
+    glVertex2f(368.0f, 120.0f); // Ajusta la coordenada x
+    glVertex2f(415.0f, 228.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+
+    glVertex2f(527.0f, 132.0f); // Ajusta la coordenada x
+    glVertex2f(452.0f, 306.0f); // Ajusta la coordenada x
+    glVertex2f(415.0f, 228.0f); // Ajusta la coordenada x
+	glEnd();
+		
+	glBegin(GL_LINE_LOOP);
+
+    glVertex2f(527.0f, 132.0f); // Ajusta la coordenada x
+    glVertex2f(452.0f, 306.0f); // Ajusta la coordenada x
+    glVertex2f(613.0f, 226.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+
+    glVertex2f(573.0f, 553.0f); // Ajusta la coordenada x
+    glVertex2f(452.0f, 306.0f); // Ajusta la coordenada x
+    glVertex2f(613.0f, 226.0f); // Ajusta la coordenada x
+	glEnd();
+		
+	glBegin(GL_LINE_LOOP);
+
+    glVertex2f(573.0f, 553.0f); // Ajusta la coordenada x
+    glVertex2f(452.0f, 306.0f); // Ajusta la coordenada x
+    glVertex2f(371.0f, 468.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+
+    glVertex2f(573.0f, 553.0f); // Ajusta la coordenada x
+    glVertex2f(528.0f, 714.0f); // Ajusta la coordenada x
+    glVertex2f(371.0f, 468.0f); // Ajusta la coordenada x
+	glEnd();
+		
+	glBegin(GL_LINE_LOOP);
+
+    glVertex2f(330.0f, 551.0f); // Ajusta la coordenada x
+	glVertex2f(461.0f, 685.0f); // Ajusta la coordenada x
+    glVertex2f(528.0f, 714.0f); // Ajusta la coordenada x
+    glVertex2f(371.0f, 468.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(330.0f, 551.0f); // Ajusta la coordenada x
+	glVertex2f(288.0f, 508.0f); // Ajusta la coordenada x
+    glVertex2f(217.0f, 571.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(261.0f, 480.0f); // Ajusta la coordenada x
+	glVertex2f(288.0f, 508.0f); // Ajusta la coordenada x
+    glVertex2f(217.0f, 571.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(261.0f, 480.0f); // Ajusta la coordenada x
+	glVertex2f(288.0f, 508.0f); // Ajusta la coordenada x
+    glVertex2f(371.0f, 468.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(464.0f, 745.0f); // Ajusta la coordenada x
+	glVertex2f(461.0f, 685.0f); // Ajusta la coordenada x
+    glVertex2f(528.0f, 714.0f); // Ajusta la coordenada x
+    glVertex2f(519.0f, 781.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	
+		glBegin(GL_LINE_LOOP);
+    glVertex2f(528.0f, 714.0f); // Ajusta la coordenada x
+	glVertex2f(573.0f, 553.0f); // Ajusta la coordenada x
+    glVertex2f(371.0f, 468.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(528.0f, 714.0f); // Ajusta la coordenada x
+	glVertex2f(573.0f, 553.0f); // Ajusta la coordenada x
+    glVertex2f(713.0f, 700.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(528.0f, 714.0f); // Ajusta la coordenada x
+	glVertex2f(699.0f, 845.0f); // Ajusta la coordenada x
+    glVertex2f(713.0f, 700.0f); // Ajusta la coordenada x
+	glEnd();
+		
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(667.0f, 885.0f); // Ajusta la coordenada x
+	glVertex2f(699.0f, 845.0f); // Ajusta la coordenada x
+    glVertex2f(528.0f, 714.0f); // Ajusta la coordenada x
+    glVertex2f(519.0f, 781.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(667.0f, 885.0f); // Ajusta la coordenada x
+	glVertex2f(519.0f, 781.0f); // Ajusta la coordenada x
+    glVertex2f(500.0f, 947.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(347.0f, 898.0f); // Ajusta la coordenada x
+	glVertex2f(519.0f, 781.0f); // Ajusta la coordenada x
+    glVertex2f(500.0f, 947.0f); // Ajusta la coordenada x
+    glVertex2f(451.0f, 951.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(347.0f, 898.0f); // Ajusta la coordenada x
+	glVertex2f(519.0f, 781.0f); // Ajusta la coordenada x
+    glVertex2f(464.0f, 745.0f); // Ajusta la coordenada x
+    glVertex2f(259.0f, 758.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(464.0f, 745.0f); // Ajusta la coordenada x
+	glVertex2f(259.0f, 758.0f); // Ajusta la coordenada x
+    glVertex2f(244.0f, 659.0f); // Ajusta la coordenada x
+	glVertex2f(461.0f, 685.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(244.0f, 659.0f); // Ajusta la coordenada x
+	glVertex2f(461.0f, 685.0f); // Ajusta la coordenada x
+	glVertex2f(330.0f, 551.0f); // Ajusta la coordenada x
+	glVertex2f(217.0f, 571.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(667.0f, 885.0f); // Ajusta la coordenada x
+    glVertex2f(500.0f, 947.0f); // Ajusta la coordenada x
+    glVertex2f(552.0f, 1211.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(552.0f, 1211.0f); // Ajusta la coordenada x
+    glVertex2f(838.0f, 1181.0f); // Ajusta la coordenada x
+    glVertex2f(630.0f, 991.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(552.0f, 1211.0f); // Ajusta la coordenada x
+    glVertex2f(838.0f, 1181.0f); // Ajusta la coordenada x
+    glVertex2f(630.0f, 991.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(752.0f, 1032.0f); // Ajusta la coordenada x
+    glVertex2f(630.0f, 991.0f); // Ajusta la coordenada x
+    glVertex2f(838.0f, 1181.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(752.0f, 1032.0f); // Ajusta la coordenada x
+    glVertex2f(630.0f, 991.0f); // Ajusta la coordenada x
+    glVertex2f(667.0f, 885.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(552.0f, 1211.0f); // Ajusta la coordenada x
+    glVertex2f(838.0f, 1181.0f); // Ajusta la coordenada x
+    glVertex2f(820.0f, 1394.0f); // Ajusta la coordenada x
+    glVertex2f(749.0f, 1396.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(451.0f, 951.0f); // Ajusta la coordenada x
+    glVertex2f(347.0f, 898.0f); // Ajusta la coordenada x
+    glVertex2f(233.0f, 1036.0f); // Ajusta la coordenada x
+    glEnd();
+    
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(233.0f, 1036.0f); // Ajusta la coordenada x
+    glVertex2f(451.0f, 951.0f); // Ajusta la coordenada x
+    glVertex2f(241.0f, 1198.0f); // Ajusta la coordenada x
+    glEnd();
+    
+    glBegin(GL_LINE_LOOP);
+    glVertex2f(443.0f, 1064.0f); // Ajusta la coordenada x
+    glVertex2f(451.0f, 951.0f); // Ajusta la coordenada x
+    glVertex2f(241.0f, 1198.0f); // Ajusta la coordenada x
+    glEnd();
+    
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(233.0f, 1036.0f); // Ajusta la coordenada x
+    glVertex2f(87.0f, 1300.0f); // Ajusta la coordenada x
+    glVertex2f(241.0f, 1198.0f); // Ajusta la coordenada x
+    glEnd();
+    
+    glBegin(GL_LINE_LOOP);
+    glVertex2f(87.0f, 1300.0f); // Ajusta la coordenada x
+    glVertex2f(23.0f, 1356.0f); // Ajusta la coordenada x
+    glVertex2f(246.0f, 1280.0f); // Ajusta la coordenada x
+    glVertex2f(443.0f, 1064.0f); // Ajusta la coordenada x
+    glEnd();
+    
+    glBegin(GL_LINE_LOOP);
+    glVertex2f(955.0f, 933.0f); // Ajusta la coordenada x
+	glVertex2f(699.0f, 845.0f); // Ajusta la coordenada x
+    glVertex2f(713.0f, 700.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(955.0f, 933.0f); // Ajusta la coordenada x
+	glVertex2f(1261.0f, 863.0f); // Ajusta la coordenada x
+    glVertex2f(713.0f, 700.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(903.0f, 460.0f); // Ajusta la coordenada x
+	glVertex2f(573.0f, 553.0f); // Ajusta la coordenada x
+    glVertex2f(713.0f, 700.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+
+    glVertex2f(573.0f, 553.0f); // Ajusta la coordenada x
+    glVertex2f(903.0f, 460.0f); // Ajusta la coordenada x
+    glVertex2f(613.0f, 226.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(903.0f, 460.0f); // Ajusta la coordenada x
+	glVertex2f(1261.0f, 863.0f); // Ajusta la coordenada x
+    glVertex2f(713.0f, 700.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(903.0f, 460.0f); // Ajusta la coordenada x
+	glVertex2f(1261.0f, 863.0f); // Ajusta la coordenada x
+    glVertex2f(1094.0f, 412.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(1405.0f, 577.0f); // Ajusta la coordenada x
+	glVertex2f(1261.0f, 863.0f); // Ajusta la coordenada x
+    glVertex2f(1094.0f, 412.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(1405.0f, 577.0f); // Ajusta la coordenada x
+	glVertex2f(1148.0f, 241.0f); // Ajusta la coordenada x
+    glVertex2f(1361.0f, 274.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(1094.0f, 412.0f); // Ajusta la coordenada x
+	glVertex2f(1148.0f, 241.0f); // Ajusta la coordenada x
+    glVertex2f(1405.0f, 577.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(1361.0f, 274.0f); // Ajusta la coordenada x
+	glVertex2f(1148.0f, 241.0f); // Ajusta la coordenada x
+    glVertex2f(1366.0f, 161.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(1205.0f, 93.0f); // Ajusta la coordenada x
+	glVertex2f(1148.0f, 241.0f); // Ajusta la coordenada x
+    glVertex2f(1366.0f, 161.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(1205.0f, 93.0f); // Ajusta la coordenada x
+	glVertex2f(1148.0f, 241.0f); // Ajusta la coordenada x
+    glVertex2f(1078.0f, 64.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(1031.0f, 180.0f); // Ajusta la coordenada x
+	glVertex2f(1148.0f, 241.0f); // Ajusta la coordenada x
+    glVertex2f(1078.0f, 64.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(1031.0f, 180.0f); // Ajusta la coordenada x
+	glVertex2f(991.0f, 112.0f); // Ajusta la coordenada x
+    glVertex2f(1078.0f, 64.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(1031.0f, 180.0f); // Ajusta la coordenada x
+	glVertex2f(1148.0f, 241.0f); // Ajusta la coordenada x
+    glVertex2f(1094.0f, 412.0f); // Ajusta la coordenada x
+    glVertex2f(903.0f, 460.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(1031.0f, 180.0f); // Ajusta la coordenada x
+	glVertex2f(815.0f, 206.0f); // Ajusta la coordenada x
+    glVertex2f(903.0f, 460.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(723.0f, 35.0f); // Ajusta la coordenada x
+	glVertex2f(815.0f, 206.0f); // Ajusta la coordenada x
+    glVertex2f(903.0f, 460.0f); // Ajusta la coordenada x
+    glVertex2f(613.0f, 226.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(723.0f, 35.0f); // Ajusta la coordenada x
+	glVertex2f(601.0f, 136.0f); // Ajusta la coordenada x
+    glVertex2f(613.0f, 226.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(723.0f, 35.0f); // Ajusta la coordenada x
+	glVertex2f(601.0f, 136.0f); // Ajusta la coordenada x
+    glVertex2f(584.0f, 37.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	glBegin(GL_LINE_LOOP);
+    glVertex2f(547.0f, 81.0f); // Ajusta la coordenada x
+	glVertex2f(601.0f, 136.0f); // Ajusta la coordenada x
+    glVertex2f(584.0f, 37.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	//---------------------------
+	
+	glBegin(GL_POLYGON);
+	glColor3f(0.0f, 0.0f, 0.0f);
+    glVertex2f(464.0f, 745.0f); // Ajusta la coordenada x
+	glVertex2f(461.0f, 685.0f); // Ajusta la coordenada x
+    glVertex2f(528.0f, 714.0f); // Ajusta la coordenada x
+    glVertex2f(519.0f, 781.0f); // Ajusta la coordenada x
+	glEnd();
+	
+	//glColor3f(0.1f, 0.1f, 0.1f);//Dark grey
+	
+	
+	
+
+}
+
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    conejo();
+    glFlush();
+}
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(1600, 1000);
+    glutCreateWindow("DIAMANTE");
+    glutDisplayFunc(display);
+   	//glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Fondo negro
+   	glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // Fondo blanco
+    glOrtho(0,1600,0,1600,-1,1);
+    glutMainLoop();
+	return 0;
+}
